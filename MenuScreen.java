025cc828595d5ffb267a7cef535d705e6a0364ae
cyc9887 <cyc9887@gmail.com>
2024-10-17 10:17:09 -0400

    added the menuscreen and 2 buttons

--- MenuScreen.java
+++ MenuScreen.java
@@ -5,5 +5,11 @@ public class MenuScreen extends World
     public MenuScreen()
     {
         super(600, 400, 1);
+        
+        Button button1 = new Button();
+        addObject(button1, 300, 300);
+        
+        Button button2 = new Button();
+        addObject(button2, 300, 100);
     }
 }
